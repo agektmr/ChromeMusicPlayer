40dcd0fe591856b85e3f0676ef76868748f95ee0
Eiji Kitamura <agektmr@gmail.com>
2012-11-05 08:27:18 +0900

    added license info etc.

--- src/js/ChromeMusicPlayer.js
+++ src/js/ChromeMusicPlayer.js
@@ -1,3 +1,20 @@
+/*
+Copyright 2012 Eiji Kitamura
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Eiji Kitamura (agektmr@gmail.com)
+*/
 var ChromeMusicPlayer = (function() {
   var mediaRoot = null;
   var mediaList = [];
@@ -23,9 +40,9 @@ var ChromeMusicPlayer = (function() {
     },
     traverse: function(root, callback) {
       filer.ls(root, (function(entries) {
-console.log(entries);
+// console.log(entries);
         entries.forEach((function(entry) {
-console.log(entry);
+// console.log(entry);
           if (entry.isDirectory) {
             this.traverse(entry, callback);
           } else {
@@ -60,7 +77,6 @@ console.log(entry);
           var tags = ID3.getAllTags(url);
           var image = '';
           if (tags.picture) {
-console.log(tags.picture);
             image = 'data:'+tags.picture.format+';base64,'+Base64.encodeBytes(tags.picture.data);
           }
           var info = {
